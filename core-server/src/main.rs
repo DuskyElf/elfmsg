f5b3c1aa119abcab439c00efe6108b358831f0ef
Rehmatpal Singh <91879372+DuskyElf@users.noreply.github.com>
2023-11-24 12:50:05 +0000

    Updated for using Vanilla Typescript (frontend) and Axum (backend) (#29)
    * Updated to vanilla Typescript
    
    * Updated to Axum
    
    * Updated README.md

--- core-server/src/main.rs
+++ core-server/src/main.rs
@@ -1,26 +1,15 @@
-#[macro_use] extern crate rocket;
+use axum::{self, routing};
 
-use rocket::State;
-use dotenv::dotenv;
-use rocket::serde::json;
+#[tokio::main]
+async fn main() {
+    let app = axum::Router::new().route("/", routing::get(hello_world));
 
-use core_server::*;
-
-#[get("/")]
-fn index() -> json::Value {
-    json::json!({
-        "response": "Hello World"
-    })
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
 
-#[launch]
-async fn rocket() -> _ {
-    dotenv().ok();
-    let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-
-    let db_pool: DbPool = sqlx::postgres::PgPool::connect(&db_url).await.unwrap();
-
-    rocket::build()
-        .manage(db_pool)
-        .mount("/", routes![index])
+async fn hello_world() -> &'static str {
+    "Hello world"
 }
